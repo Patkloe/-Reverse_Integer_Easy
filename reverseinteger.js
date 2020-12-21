7349df48d166d6805d632d69a056805739fe1713
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-12-21 10:45:34 -0600

    Update reverseinteger.js

--- reverseinteger.js
+++ reverseinteger.js
@@ -1,15 +1,12 @@
  var reverse = function(x) {
-    
+     var ret = '';
         var pos = true;
         if(x > 0 ){ pos = true;}else{ pos = false;}
            if(pos){
               var test = Number(x).toString();
-               console.log(test);
-               var ret = '';
                for(var i = test.length -1 ; i >=0 ; i--){
                  ret = ret + test[i];
                 }
-                console.log("retourne :" + " " + ret);
                if(Number(ret) > Math.pow(2,31)){
                    return 0;
                }
@@ -19,7 +16,6 @@
            }
            else{
                 var conv = (-1)*(x);
-                var ret = '';
                 var test = Number(conv).toString();
                for(var i = test.length -1 ; i >=0 ; i--){
                  ret = ret + test[i];
